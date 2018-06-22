3669438376916e706167b8677065fa15ebed6e7c
Ford Higgins <fordhiggins@Fords-MacBook-Pro.local>
2018-06-21 20:56:41 -0700

    thursday update

--- logistic_regression_torch.py
+++ logistic_regression_torch.py
@@ -14,15 +14,58 @@ class LogisticRegression(nn.Module):
 
 
 	def forward(self, x):
-		out = self.linear(x)
+		out = self.linear(x) # is this what I want? or wrap it in F.log_softmax()? 
 		return out
 
+input_dim = 28
+input_size = input_dim**2
+n_classes = 10
+n_epochs = 10
+learning_rate = 0.01
 
 # Creating the model object
-model = LogisticRegression(input_size, num_classes)
+model = LogisticRegression(input_size, n_classes)
 
 # Initiating loss and optimizer
 # do we want SGD or Adam? read up on differences
 # want something else for loss? CEL computes softmax internally
 loss = nn.CrossEntropyLoss() 
-optimizer = optim.SGD(model.parameters(), lr = learning_rate)
+
+def train_epochs(model, train_dl = train_dl, n_epochs, lr = 0.01, wd = 0.0):
+	parameters = filter(lambda p: p.requires_grad, model.parameters()) # is this necessary?
+	optimizer = optim.SGD(model.parameters, lr = lr)
+	model.train()
+	for epoch in torch.range(n_epochs):
+		for i, (x, y) in enumerate(train_dl):
+			out = model(x)
+			loss = F.cross_entropy(out, y)
+
+			optimizer.zero_grad()
+			loss.backward()
+			optimizer.step()
+
+		if epoch % 2 == 0:
+			print('Epoch ' + epoch + ': ' loss.item())
+	test_loss(model, test_dl)
+
+
+def test_loss(model, test_dl = test_dl):
+	model.eval()
+	total = 0
+	correct = 0
+	preds_list = list()
+
+	for i, (x, y) in enumerate(test_dl):
+		batch = y.shape[0]
+		out = model(x)
+		loss = F.cross_entropy(out, y)
+		preds = torch.argmax(out.data)
+
+		correct += preds.eq(y.data).sum().item()
+		total += batch
+		preds_list.append(pred)
+    print("val loss and accuracy", sum_loss/total, correct/total)
+    return preds_list
+
+
+
